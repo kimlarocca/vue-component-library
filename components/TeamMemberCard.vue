48865bad61bd922f567e103628c9a8ed29f2e599
Kim LaRocca <kimlarocca@me.com>
2019-09-26 14:57:49 -0400

    updated team member card

--- components/TeamMemberCard.vue
+++ components/TeamMemberCard.vue
@@ -1,16 +1,17 @@
 <template>
 
-  <div class="team-member-card">
+  <div class="team-member-card" :class="layout">
 
     <div class="grid-x grid-margin-x grid-margin-y">
-      <div v-if="hasImage" class="cell large-2 medium-4 small-12">
+      <div v-if="hasImage" :class="isHorizontal ? 'large-2 medium-4 small-12' : 'medium-12'" class="cell">
         <img :src="image" :alt="altText"/>
       </div>
       <div class="cell large-auto medium-auto small-12">
         <p v-if="hasName" class="name">{{ name }}</p>
         <p v-if="hasTitle" class="title">{{ title }}</p>
         <slot></slot>
-        <a v-if="hasLink" :href="link" class="button">{{ cta }}</a>
+        <a v-if="hasLink && !hasTextLink" :href="link" class="button">{{ cta }}</a>
+        <a v-if="hasLink && hasTextLink" :href="link" class="caret-right">{{ cta }}</a>
       </div>
     </div>
 
@@ -27,7 +28,15 @@
       altText: String,
       title: String,
       cta: String,
-      link: String
+      link: String,
+      hasTextLink: {
+        type: Boolean,
+        default: false
+      },
+      layout: {
+        type: String,
+        default: 'vertical'
+      }
     },
     computed: {
       hasHeader () {
@@ -44,6 +53,9 @@
       },
       hasTitle () {
         return !!this.$props['title']
+      },
+      isHorizontal () {
+        return this.$props['layout'] === 'horizontal'
       }
     }
   }
