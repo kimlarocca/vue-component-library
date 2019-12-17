190daeab052a87594491ace61de9bbfbb5ac522f
Andres-A <andresar11@gmail.com>
2019-12-17 13:07:58 -0500

    Biography Card Component/Pages file updated

--- components/BiographyCard.vue
+++ components/BiographyCard.vue
@@ -1,62 +1,62 @@
 <template>
-
   <div class="team-member-card" :class="layout">
-
     <div class="grid-x grid-margin-x grid-margin-y">
-      <div v-if="hasImage" :class="isHorizontal ? 'large-2 medium-4 small-12' : 'medium-12'" class="cell">
-        <img :src="image" :alt="altText"/>
+      <div
+        v-if="hasImage"
+        :class="isHorizontal ? 'large-2 medium-4 small-12' : 'medium-12'"
+        class="cell"
+      >
+        <img :src="image" :alt="altText" />
       </div>
       <div class="cell large-auto medium-auto small-12">
-        <p v-if="hasName" class="name">{{ name }}</p>
-        <p v-if="hasTitle" class="title">{{ title }}</p>
+        <div v-if="hasName" class="name">{{ name }}</div>
+        <div v-if="hasTitle" class="title like-p">{{ title }}</div>
         <slot></slot>
         <a v-if="hasLink && !hasTextLink" :href="link" class="button">{{ cta }}</a>
         <a v-if="hasLink && hasTextLink" :href="link" class="caret-right">{{ cta }}</a>
       </div>
     </div>
-
   </div>
-
 </template>
 
 <script>
-  export default {
-    name: 'TeamMemberCard',
-    props: {
-      name: String,
-      image: String,
-      altText: String,
-      title: String,
-      cta: String,
-      link: String,
-      hasTextLink: {
-        type: Boolean,
-        default: false
-      },
-      layout: {
-        type: String,
-        default: 'vertical'
-      }
+export default {
+  name: "TeamMemberCard",
+  props: {
+    name: String,
+    image: String,
+    altText: String,
+    title: String,
+    cta: String,
+    link: String,
+    hasTextLink: {
+      type: Boolean,
+      default: false
     },
-    computed: {
-      hasHeader () {
-        return !!this.$props['header']
-      },
-      hasImage () {
-        return !!this.$props['image']
-      },
-      hasLink () {
-        return !!this.$props['link']
-      },
-      hasName () {
-        return !!this.$props['name']
-      },
-      hasTitle () {
-        return !!this.$props['title']
-      },
-      isHorizontal () {
-        return this.$props['layout'] === 'horizontal'
-      }
+    layout: {
+      type: String,
+      default: "vertical"
+    }
+  },
+  computed: {
+    hasHeader() {
+      return !!this.$props["header"];
+    },
+    hasImage() {
+      return !!this.$props["image"];
+    },
+    hasLink() {
+      return !!this.$props["link"];
+    },
+    hasName() {
+      return !!this.$props["name"];
+    },
+    hasTitle() {
+      return !!this.$props["title"];
+    },
+    isHorizontal() {
+      return this.$props["layout"] === "horizontal";
     }
   }
+};
 </script>
